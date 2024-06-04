17d3f108aa5cb12915bd7a019fd9c86977f3e3da
Tatenda <31291528+rideam@users.noreply.github.com>
2024-06-05 01:18:42 +0200

    initial app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import './index.css'
+import './index.css';
 import ReactDOM from 'react-dom/client';
-import SimpleGrid from "./SimpleGrid";
+import SimpleGrid from './SimpleGrid';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <SimpleGrid />
-  </React.StrictMode>
+    <React.StrictMode>
+        <SimpleGrid />
+    </React.StrictMode>
 );
-
